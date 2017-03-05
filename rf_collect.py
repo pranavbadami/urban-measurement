704186213c51c5ce5bc51022be2fab3597e6a7b1
pranavbadami <pranavbadami@gmail.com>
2017-03-05 03:13:05 +0000

    basic logging of signal strength

--- rf_collect.py
+++ rf_collect.py
@@ -8,86 +8,138 @@
 # Author: Pranav Badami (adapted from RFExplore-for-Python)
 #=========================================================
 
+import math
 import time
 from datetime import datetime, timedelta
 import RFExplorer
+import csv
 
 #---------------------------------------------------------
 # global variables and initialization
 #---------------------------------------------------------
 
-#SERIALPORT = "/dev/cu.SLAB_USBtoUART"    #serial port identifier, use None to autodetect
+#SERIALPORT = "/dev/cu.SLAB_USBtoUART"	  #serial port identifier, use None to autodetect
 BAUDRATE = 500000
 SERIALPORT = "/dev/ttyUSB0"
-objRFE = RFExplorer.RFECommunicator()     #Initialize object and thread
-TOTAL_SECONDS = 10           #Initialize time span to display activity
-PRINT_COUNT = 0
+objRFE = RFExplorer.RFECommunicator()	  #Initialize object and thread
+TOTAL_SECONDS = 10			 #Initialize time span to display activity
+MIN_FREQ = 0
+MAX_FREQ = 0
+STEP_FREQ = 0
+center_fq = 2442
+
 #---------------------------------------------------------
 # Helper functions
 #---------------------------------------------------------
 
 def PrintData(objRFE):
-    """This function prints the amplitude and frequency peak of the latest received sweep
+	"""This function prints the amplitude and frequency peak of the latest received sweep
 	"""
-    nInd = objRFE.SweepData.Count-1
-    objSweepTemp = objRFE.SweepData.GetData(nInd)
-    print("in PrintData")
-    print("start freq", objSweepTemp.m_fStartFrequencyMHZ, "end freq", objSweepTemp.m_fStartFrequencyMHZ + objSweepTemp.m_fStepFrequencyMHZ*len(objSweepTemp.m_arrAmplitude))
-    #nStep = objSweepTemp.GetPeakStep()      #Get index of the peak
-    #fAmplitudeDBM = objSweepTemp.GetAmplitude_DBM(nStep)    #Get amplitude of the peak
-    #fCenterFreq = objSweepTemp.GetFrequencyMHZ(nStep)   #Get frequency of the peak
+	nInd = objRFE.SweepData.Count-1
+	objSweepTemp = objRFE.SweepData.GetData(nInd)
+	print("in PrintData")
+	print("start freq", objSweepTemp.m_fStartFrequencyMHZ, "end freq", objSweepTemp.m_fStartFrequencyMHZ + objSweepTemp.m_fStepFrequencyMHZ*len(objSweepTemp.m_arrAmplitude))
+	#nStep = objSweepTemp.GetPeakStep()		 #Get index of the peak
+	#fAmplitudeDBM = objSweepTemp.GetAmplitude_DBM(nStep)	 #Get amplitude of the peak
+	#fCenterFreq = objSweepTemp.GetFrequencyMHZ(nStep)	 #Get frequency of the peak
 
-    #print("Sweep[" + str(nInd)+"]: Peak: " + "{0:.3f}".format(fCenterFreq) + "MHz  " + str(fAmplitudeDBM) + "dBm")
+	#print("Sweep[" + str(nInd)+"]: Peak: " + "{0:.3f}".format(fCenterFreq) + "MHz	" + str(fAmplitudeDBM) + "dBm")
+
+def sweep_index(freq):
+	"""
+	Helper function to calculate index of frequency 
+	in sweep amplitude array. Index 0 is the min 
+	frequency and each subsequent index adds STEP_FREQ
+	"""
+	diff = freq - MIN_FREQ
+	index = math.floor(diff/STEP_FREQ)
+	return index
+
+def signal_strength(objRFE, freq):
+	"""
+	This function takes a target frequency and gets the 
+	corresponding signal strength from sweep data.
+	"""
+	global MIN_FREQ
+	global STEP_FREQ
+	global MAX_FREQ
+
+	nInd = objRFE.SweepData.Count - 1
+	objSweepTemp = objRFE.SweepData.GetData(nInd)
+
+	#initialize constants, if needed
+	if not MIN_FREQ:
+		MIN_FREQ = objSweepTemp.m_fStartFrequencyMHZ
+		STEP_FREQ = objSweepTemp.m_fStepFrequencyMHZ
+		MAX_FREQ = MIN_FREQ + STEP_FREQ*len(objSweepTemp.m_arrAmplitude)
+		MAX_FREQ = round(MAX_FREQ, 0)
+
+	if (freq < MIN_FREQ) or (freq > MAX_FREQ):
+		print("Invalid frequency")
+		return None
+
+	else:
+		index = sweep_index(freq)
+		return objSweepTemp.m_arrAmplitude[index]	
+
 
 #---------------------------------------------------------
 # Main processing loop
 #---------------------------------------------------------
 
 try:
-    #Find and show valid serial ports
-    objRFE.GetConnectedPorts()    
+	#Find and show valid serial ports
+	objRFE.GetConnectedPorts()	  
 
-    #Connect to available port
-    if (objRFE.ConnectPort(SERIALPORT, BAUDRATE)):
-        #Reset the unit to start fresh
-        objRFE.SendCommand("r")
-        #Wait for unit to notify reset completed
-        while(objRFE.IsResetEvent):
-            pass
-        #Wait for unit to stabilize
-        time.sleep(3)
+	#Connect to available port
+	if (objRFE.ConnectPort(SERIALPORT, BAUDRATE)):
+ 
+		#open log file
+		fields = ['frequency', 'dBm']
+		log = open('data/test.csv', 'w')
+		writer = csv.DictWriter(log, fieldnames=fields)
+		writer.writeheader()
 
-        #Request RF Explorer configuration
-        objRFE.SendCommand_RequestConfigData()
-        #Wait to receive configuration and model details
-        while(objRFE.ActiveModel == RFExplorer.RFE_Common.eModel.MODEL_NONE):
-            objRFE.ProcessReceivedString(True)    #Process the received configuration
+		#Reset the unit to start fresh
+		objRFE.SendCommand("r")
+		#Wait for unit to notify reset completed
+		while(objRFE.IsResetEvent):
+			pass
+		#Wait for unit to stabilize
+		time.sleep(3)
 
-        #If object is an analyzer, we can scan for received sweeps
-        if (objRFE.IsAnalyzer()):     
-            print("Receiving data...")
-            #Process until we complete scan time
-            nLastDisplayIndex=0
-            startTime=datetime.now()
-            while ((datetime.now() - startTime).seconds<TOTAL_SECONDS):    
-                #Process all received data from device 
-            	objRFE.ProcessReceivedString(True)
-                #Print data if received new sweep only
-            	if (objRFE.SweepData.Count>nLastDisplayIndex):
-                	PrintData(objRFE)
-                	PRINT_COUNT += 1      
-            	nLastDisplayIndex=objRFE.SweepData.Count
-            	if (PRINT_COUNT): break
-        else:
-            print("Error: Device connected is a Signal Generator. \nPlease, connect a Spectrum Analyzer")
-    else:
-        print("Not Connected")
+		#Request RF Explorer configuration
+		objRFE.SendCommand_RequestConfigData()
+		#Wait to receive configuration and model details
+		while(objRFE.ActiveModel == RFExplorer.RFE_Common.eModel.MODEL_NONE):
+			objRFE.ProcessReceivedString(True)	  #Process the received configuration
+
+		#If object is an analyzer, we can scan for received sweeps
+		if (objRFE.IsAnalyzer()):
+			print("Receiving data...")
+			#Process until we complete scan time
+			nLastDisplayIndex=0
+			startTime=datetime.now()
+			while ((datetime.now() - startTime).seconds<TOTAL_SECONDS):    
+				#Process all received data from device 
+				objRFE.ProcessReceivedString(True)
+				#Log data if received new sweep only
+				if (objRFE.SweepData.Count>nLastDisplayIndex):
+					dBm = signal_strength(objRFE, center_fq)
+					if dBm is None:
+						break
+					writer.writerow({'frequency': center_fq, 'dBm': dBm})
+				nLastDisplayIndex=objRFE.SweepData.Count
+		else:
+			print("Error: Device connected is a Signal Generator. \nPlease, connect a Spectrum Analyzer")
+	else:
+		print("Not Connected")
 except Exception as obEx:
-    print("Error: " + str(obEx))
+	print("Error: " + str(obEx))
 
 #---------------------------------------------------------
 # Close object and release resources
 #---------------------------------------------------------
 
-objRFE.Close()    #Finish the thread and close port
+objRFE.Close()	  #Finish the thread and close port
 objRFE = None 
