c08d392e2fcd2b3c55f9534fd1e5f57f6d96c340
pranavbadami <pranavbadami@gmail.com>
2017-03-06 18:29:57 +0000

    issue with logging

--- log.py
+++ log.py
@@ -16,17 +16,10 @@ ser = serial.Serial(
 
 count = 0
 
-num_log = len(glob.glob1("/home/pi/urban-measurement/", "log*.txt"))
+num_log = len(glob.glob1("/home/pi/urban-measurement/data/", "log*.txt"))
 
-with open("/home/pi/urban-measurement/log" +str(num_log) + ".txt", "wb") as f:
+with open("/home/pi/urban-measurement/data/log" +str(num_log) + ".txt", "wb") as f:
 	while 1:
 		x = ser.readline()
-		#convert to string gps_data
-		bytes.decode(x)?
-		msg = pynmea2.parse(gps_data)
-		if msg class == RMC and msg.status == "A"
-			msg.lon
-			msg.lat
-
 		f.write(x)
 		f.write(str.encode("\n"))	
