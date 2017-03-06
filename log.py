87348c945fb363963da47b64f80b39747bc0ae8a
pranavbadami <pranavbadami@gmail.com>
2017-03-06 01:58:05 +0000

    function gps read script

---
+++ log.py
@@ -0,0 +1,24 @@
+import time
+import serial
+import glob
+
+time.sleep(10)
+
+ser = serial.Serial(
+	port = '/dev/ttyAMA0',
+	baudrate = 9600,
+	parity = serial.PARITY_NONE,
+	stopbits = serial.STOPBITS_ONE,
+	bytesize = serial.EIGHTBITS,
+	timeout = 1
+)
+
+count = 0
+
+num_log = len(glob.glob1("/home/pi/", "log*.txt"))
+
+with open("/home/pi/log" +str(num_log) + ".txt", "wb") as f:
+	while 1:
+		x = ser.readline()	
+		f.write(x)
+		f.write(str.encode("\n"))	
